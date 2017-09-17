f21f921cb947d4d8eb9f3710a9f94445b6e790d4
zht198484 <zht198484@163.com>
2017-09-17 22:04:46 +0800

    Initial commit for java web start

---
+++ src/main/java/com/zht198484/java_web_start/HelloServlet.java
@@ -0,0 +1,26 @@
+package com.zht198484.java_web_start;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Created by zht198484 on 2017/9/17.
+ * Hello World Servlet
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DateTimeFormatter dtf = DateTimeFormatter.ISO_DATE_TIME;
+        LocalDateTime ldt = LocalDateTime.now();
+        String time = ldt.format(dtf);
+        req.setAttribute("currentTime",time);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp);
+    }
+}
